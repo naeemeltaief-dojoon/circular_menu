d287bbd2e21a758682f34bac617deca778ecca5e
Naeem Eltaief <n.eltaief@dojoon.ly>
2023-10-31 11:56:33 +0200

    Update circular_menu_item.dart

--- lib/src/circular_menu_item.dart
+++ lib/src/circular_menu_item.dart
@@ -71,8 +71,8 @@ class CircularMenuItem extends StatelessWidget {
         child: Material(
           color: color ?? Theme.of(context).primaryColor,
           child: SizedBox(
-            width: 80,
-            height: 80,
+            width: 70,
+            height: 70,
             child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.grey,
