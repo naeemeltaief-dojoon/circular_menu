4de2d28609822226df2942c7bd339889c6df2cc0
naeemeltaief <n.eltaief@dojoon.ly>
2024-03-15 06:25:57 +0200

    Update main.dart

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,79 +22,79 @@ class _MyAppState extends State<MyApp> {
           backgroundColor: Colors.pink,
           title: Text('Flutter Circular Menu'),
         ),
-        body: CircularMenu(
-          alignment: Alignment.bottomCenter,
-          toggleButtonMargin: 10,
-          toggleButtonPadding: 10,
-          toggleButtonSize: 40,
-          startingAngleInRadian: 22.5,
-          endingAngleInRadian: 12,
-          backgroundWidget: Center(
-            child: RichText(
-              text: TextSpan(
-                style: TextStyle(color: Colors.black, fontSize: 28),
-                children: <TextSpan>[
-                  TextSpan(
-                    text: _colorName,
-                    style:
-                        TextStyle(color: _color, fontWeight: FontWeight.bold),
-                  ),
-                  TextSpan(text: ' button is clicked.'),
-                ],
+        body: Center(
+          child: CircularMenu(
+            alignment: Alignment.topCenter,
+            toggleButtonMargin: 10,
+            toggleButtonPadding: 10,
+            toggleButtonSize: 40,
+            backgroundWidget: Center(
+              child: RichText(
+                text: TextSpan(
+                  style: TextStyle(color: Colors.black, fontSize: 28),
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: _colorName,
+                      style:
+                          TextStyle(color: _color, fontWeight: FontWeight.bold),
+                    ),
+                    TextSpan(text: ' button is clicked.'),
+                  ],
+                ),
               ),
             ),
+            toggleButtonColor: Colors.pink,
+            toggleButtonLabel: Text('Click'),
+            items: [
+              CircularMenuItem(
+                iconSize: 30,
+                buttonLabel: Text('Home'),
+                  icon: Icons.home,
+                  color: Colors.green,
+                  onTap: () {
+                    setState(() {
+                      _color = Colors.green;
+                      _colorName = 'Green';
+                    });
+                  }),
+              CircularMenuItem(
+                  icon: Icons.search,
+                  color: Colors.blue,
+                  onTap: () {
+                    setState(() {
+                      _color = Colors.blue;
+                      _colorName = 'Blue';
+                    });
+                  }),
+              // CircularMenuItem(
+              //     icon: Icons.settings,
+              //     color: Colors.orange,
+              //     onTap: () {
+              //       setState(() {
+              //         _color = Colors.orange;
+              //         _colorName = 'Orange';
+              //       });
+              //     }),
+              // CircularMenuItem(
+              //     icon: Icons.chat,
+              //     color: Colors.purple,
+              //     onTap: () {
+              //       setState(() {
+              //         _color = Colors.purple;
+              //         _colorName = 'Purple';
+              //       });
+              //     }),
+              // CircularMenuItem(
+              //     icon: Icons.notifications,
+              //     color: Colors.brown,
+              //     onTap: () {
+              //       setState(() {
+              //         _color = Colors.brown;
+              //         _colorName = 'Brown';
+              //       });
+              //     })
+            ],
           ),
-          toggleButtonColor: Colors.pink,
-          toggleButtonLabel: Text('Click'),
-          items: [
-            CircularMenuItem(
-              iconSize: 30,
-              buttonLabel: Text('Home'),
-                icon: Icons.home,
-                color: Colors.green,
-                onTap: () {
-                  setState(() {
-                    _color = Colors.green;
-                    _colorName = 'Green';
-                  });
-                }),
-            CircularMenuItem(
-                icon: Icons.search,
-                color: Colors.blue,
-                onTap: () {
-                  setState(() {
-                    _color = Colors.blue;
-                    _colorName = 'Blue';
-                  });
-                }),
-            // CircularMenuItem(
-            //     icon: Icons.settings,
-            //     color: Colors.orange,
-            //     onTap: () {
-            //       setState(() {
-            //         _color = Colors.orange;
-            //         _colorName = 'Orange';
-            //       });
-            //     }),
-            // CircularMenuItem(
-            //     icon: Icons.chat,
-            //     color: Colors.purple,
-            //     onTap: () {
-            //       setState(() {
-            //         _color = Colors.purple;
-            //         _colorName = 'Purple';
-            //       });
-            //     }),
-            // CircularMenuItem(
-            //     icon: Icons.notifications,
-            //     color: Colors.brown,
-            //     onTap: () {
-            //       setState(() {
-            //         _color = Colors.brown;
-            //         _colorName = 'Brown';
-            //       });
-            //     })
-          ],
         ),
       ),
     );
